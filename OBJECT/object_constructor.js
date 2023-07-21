74d87af53910451646e4d8ca5869e32a0771c6fc
vietngd <viet.ngd.2002@gmail.com>
2023-07-21 15:09:12 +0700

    push

--- OBJECT/object_constructor.js
+++ OBJECT/object_constructor.js
@@ -5,15 +5,18 @@ function User(name, fullName, age, address) {
   this.fullName = fullName;
   this.age = age;
   this.address = address;
+  // khoi tao 1 funcion trong 1 others
   this.getName = function () {
-    return `${this.fullName} ${this.name}`; // this: khi goi den property naof thi this se la cai do
+    return `${this.name} ${this.fullName}`;
   };
 }
+
 // new + object constructor
 var student = new User("Nga", "Nguyen", 20, "BAcNinh");
 var teacher = new User("Dam", "Tran", 50, "HaNoi");
-
-console.log(student.getName());
-
+console.log(teacher.getName()); //goi toi ham getName thi funcion duoc thuc thi
+teacher.mail = "Dam@gmail.com";
+delete teacher.mail; // xoa 1 others trong methods
 student.mail = "nga@gmai.com"; // tao them thuoc tinh cho student( khong anh huong den ban thiet k ban dau)
 console.log(teacher);
+console.log(student);
